835d62cdf36c7d7aad249850ce4eb43f260d5aa8
yuszuth <yusufchik2004@mail.ru>
2020-12-03 15:23:57 +0300

    final_task1'

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from flask import Flask
-import ra
+import urllib
+
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
+def menu():
+    f = open("links.html")
+    out = "<pre>{}</pre>".format("\n".join(f))
+    return out
+
 @app.route('/haba')
 def hello_world():
-
     s = ["Hello, Haba!",
-        "Hello, Arsen!",
-        "Hello, Karim!",]
+         "Hello, Arsen!",
+         "Hello, Karim!", ]
 
     out = "<pre>{}</pre>".format("\n".join(s))
     return out
 
+
 @app.route('/random')
-def random():
-	print("Haba's mark if ", sep="", end="")
-	print(random.randint(0, 100))
+def rand():
+    s = [f"Haba's mark if {random.randint(0, 100)}"]
+
+    sec_out = "<pre>{}</pre>".format("\n".join(s))
+    return sec_out
+
+
+@app.route('/i_will_not')
+def willnot():
+    s = ["I will not waste time"] * 100
+
+    third_out = "<pre>{}</pre>".format("\n".join(s))
+    return third_out
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
